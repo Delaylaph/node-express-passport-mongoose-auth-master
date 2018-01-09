7a249f358bb5f41f1fe176cb20c32ae0e70606e6
ZzzerOo4 <ZzzerOo4@yandex.ru>
2018-01-09 15:12:46 +0300

    Add files via upload

---
+++ routes/movie.js
@@ -0,0 +1,14 @@
+const url = require('url');
+const omdb = require('../lib/omdb');
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function(req, res) {
+    (req, res)=> {
+
+    }
+});
+
+module.exports = router;
+
